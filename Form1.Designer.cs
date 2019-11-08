74fef9de567f245d8a0580d9c4feb5abda2ff786
garthbowden@gmail.com <garthbowden@gmail.com>
2019-11-08 12:44:07 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace UDPChatClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendBroadcast = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbLocalPort = new System.Windows.Forms.TextBox();
+            this.tbRemotePort = new System.Windows.Forms.TextBox();
+            this.tbBroadcastText = new System.Windows.Forms.TextBox();
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSendBroadcast
+            // 
+            this.btnSendBroadcast.Location = new System.Drawing.Point(11, 107);
+            this.btnSendBroadcast.Name = "btnSendBroadcast";
+            this.btnSendBroadcast.Size = new System.Drawing.Size(224, 23);
+            this.btnSendBroadcast.TabIndex = 0;
+            this.btnSendBroadcast.Text = "Send Broadcast";
+            this.btnSendBroadcast.UseVisualStyleBackColor = true;
+            this.btnSendBroadcast.Click += new System.EventHandler(this.BtnSendBroadcast_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Local Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Remote Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Text";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // tbLocalPort
+            // 
+            this.tbLocalPort.Location = new System.Drawing.Point(101, 3);
+            this.tbLocalPort.Name = "tbLocalPort";
+            this.tbLocalPort.Size = new System.Drawing.Size(115, 22);
+            this.tbLocalPort.TabIndex = 4;
+            this.tbLocalPort.Text = "23000";
+            // 
+            // tbRemotePort
+            // 
+            this.tbRemotePort.Location = new System.Drawing.Point(101, 34);
+            this.tbRemotePort.Name = "tbRemotePort";
+            this.tbRemotePort.Size = new System.Drawing.Size(115, 22);
+            this.tbRemotePort.TabIndex = 5;
+            this.tbRemotePort.Text = "23000";
+            // 
+            // tbBroadcastText
+            // 
+            this.tbBroadcastText.Location = new System.Drawing.Point(60, 79);
+            this.tbBroadcastText.Name = "tbBroadcastText";
+            this.tbBroadcastText.Size = new System.Drawing.Size(175, 22);
+            this.tbBroadcastText.TabIndex = 6;
+            this.tbBroadcastText.Text = "default text";
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Location = new System.Drawing.Point(241, 8);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.Size = new System.Drawing.Size(552, 262);
+            this.tbConsole.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 282);
+            this.Controls.Add(this.tbConsole);
+            this.Controls.Add(this.tbBroadcastText);
+            this.Controls.Add(this.tbRemotePort);
+            this.Controls.Add(this.tbLocalPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSendBroadcast);
+            this.Name = "Form1";
+            this.Text = "Client Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendBroadcast;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbLocalPort;
+        private System.Windows.Forms.TextBox tbRemotePort;
+        private System.Windows.Forms.TextBox tbBroadcastText;
+        private System.Windows.Forms.TextBox tbConsole;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UDPAsynchronousChat;
+
+namespace UDPChatClientForm
+{
+    public partial class Form1 : Form
+    {
+        UDPAsynchronousChat.UDPAsyncChatClient mClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSendBroadcast_Click(object sender, EventArgs e)
+        {
+            if (mClient == null)
+            {
+                if (int.TryParse(tbLocalPort.Text, out int localPort)
+                    && int.TryParse(tbRemotePort.Text, out int RemotePort)
+                    && localPort >= 0 && localPort <= 65535
+                    && RemotePort >= 0 && RemotePort <= 65535)
+                {
+                    mClient = new UDPAsynchronousChat.UDPAsyncChatClient(localPort, RemotePort);
+                    mClient.RaiseTextUpdateEvent += ConsoleTextUpdate;
+
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid port values: Local Port = {tbLocalPort.Text}; Remote Port = {tbRemotePort.Text}");
+                }
+            }
+            mClient.SendBroadcast(tbBroadcastText.Text);
+        }
+
+        private void ConsoleTextUpdate(object sender, TextUpdateEventArgs e)
+        {
+            tbConsole.AppendText(e.Text + Environment.NewLine);
+            Console.WriteLine(e.Text);
+        }
+    }
+}
